16f8269af225e40904935aeea70fa1dd1a62ceff
Alex Bulintis <alex@bulintis.com>
2022-04-26 21:47:33 +0300

    prettier

--- reusables/generateIntroduction.tsx
+++ reusables/generateIntroduction.tsx
@@ -1,8 +1,8 @@
-import { userPersonalData } from "../types";
+import { userPersonalData } from '../types';
 
 const generateIntroduction = (personal: userPersonalData) => {
   const { nume, prenume, adresaLinie1, localitate, judet } = personal;
   return `Subsemnatul/a ${nume} ${prenume}, domiciliat/ă în ${localitate}, ${judet}, ${adresaLinie1}, formulez prezenta sesizare:`;
-}
+};
 
 export default generateIntroduction;
