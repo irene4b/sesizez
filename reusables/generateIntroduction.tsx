aa230105be63c533452202e788031831bf3ef501
Alex Bulintis <alex@bulintis.com>
2022-04-26 21:47:21 +0300

    fix typo

--- reusables/generateIntroduction.tsx
+++ reusables/generateIntroduction.tsx
@@ -2,7 +2,7 @@ import { userPersonalData } from "../types";
 
 const generateIntroduction = (personal: userPersonalData) => {
   const { nume, prenume, adresaLinie1, localitate, judet } = personal;
-  return `Sumbsemnatul/a ${nume} ${prenume}, domiciliat/ă în ${localitate}, ${judet}, ${adresaLinie1}, formulez prezenta sesizare:`;
+  return `Subsemnatul/a ${nume} ${prenume}, domiciliat/ă în ${localitate}, ${judet}, ${adresaLinie1}, formulez prezenta sesizare:`;
 }
 
 export default generateIntroduction;
