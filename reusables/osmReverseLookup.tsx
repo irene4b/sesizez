31ddd07eb30539b66cd81206021b708b215522a0
Alex Bulintis <alex@bulintis.com>
2022-04-30 12:57:00 +0300

    improve location

--- reusables/osmReverseLookup.tsx
+++ reusables/osmReverseLookup.tsx
@@ -17,11 +17,17 @@ export const osmReverseLookup = async (props: Props): Promise<reportedLocation> 
     throw new Error('Nu s-a putut găsi adresa');
   };
 
+  console.log(response.data.address);
+
   let adresaLinie1 = '';
   if(response.data.address.road) adresaLinie1 += `${response.data.address.road} `;
   if(response.data.address.house_number) adresaLinie1 += `nr. ${response.data.address.house_number}`;
-  if(response.data.addresstype === 'building') adresaLinie1 += ` (pe langa ${response.data.address.building})`;
   
+  // adauga reper
+  if(response.data.addresstype === 'building') adresaLinie1 += ` (pe langa ${response.data.address.building})`;
+  else if(response.data.address.shop) adresaLinie1 += ` (pe langa ${response.data.address.shop})`;
+  else if(response.data.address.amenity) adresaLinie1 += ` (pe langa ${response.data.address.amenity})`;
+
   let city = '';
   if(response.data.address.city) city = response.data.address.city;
   if(response.data.address.town) city = response.data.address.town;
@@ -30,6 +36,7 @@ export const osmReverseLookup = async (props: Props): Promise<reportedLocation> 
   let county = '';
   if(response.data.address.county) county = response.data.address.county;
   if(response.data.address.city_district) county = response.data.address.city_district;
+  else if(response.data.address.district) county = response.data.address.district;
 
   return {
     adresaLinie1,
