877c29f51f484b692ac41c4a726994a1f39960b9
Alex Bulintis <alex@bulintis.com>
2023-01-06 17:08:13 +0200

    bug fixes

--- reusables/osmReverseLookup.tsx
+++ reusables/osmReverseLookup.tsx
@@ -1,42 +1,63 @@
-import axios from "axios";
-import { Alert } from "react-native";
-import { reportedLocation } from "../types";
+import axios from 'axios';
+import { Alert } from 'react-native';
+import { reportedLocation } from '../types';
 
 type Props = {
   lat: number;
   lng: number;
-}
+};
 
-export const osmReverseLookup = async (props: Props): Promise<reportedLocation> => {
+export const osmReverseLookup = async (
+  props: Props
+): Promise<reportedLocation> => {
   const { lat, lng } = props;
   const url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}`;
-  const response = await axios.get(url);
+  const response = await axios.get(url, {
+    headers: {
+      'accept-language': 'en-US',
+    },
+  });
 
-  if(!response.data.address) {
+  if (!response.data.address) {
     Alert.alert('Nu s-a putut găsi adresa');
     throw new Error('Nu s-a putut găsi adresa');
-  };
+  }
 
   console.log(response.data.address);
 
   let adresaLinie1 = '';
-  if(response.data.address.road) adresaLinie1 += `${response.data.address.road} `;
-  if(response.data.address.house_number) adresaLinie1 += `nr. ${response.data.address.house_number}`;
-  
+  if (response.data.address.road)
+    adresaLinie1 += `${response.data.address.road} `;
+  if (response.data.address.house_number)
+    adresaLinie1 += `nr. ${response.data.address.house_number}`;
+
   // adauga reper
-  if(response.data.addresstype === 'building') adresaLinie1 += ` (pe langa ${response.data.address.building})`;
-  else if(response.data.address.shop) adresaLinie1 += ` (pe langa ${response.data.address.shop})`;
-  else if(response.data.address.amenity) adresaLinie1 += ` (pe langa ${response.data.address.amenity})`;
+  if (
+    response.data.addresstype === 'building' &&
+    response.data.address.building
+  )
+    adresaLinie1 += ` (pe langa ${response.data.address.building})`;
+  else if (response.data.address.shop)
+    adresaLinie1 += ` (pe langa ${response.data.address.shop})`;
+  else if (response.data.address.amenity)
+    adresaLinie1 += ` (pe langa ${response.data.address.amenity})`;
+  else {
+    adresaLinie1 += ``;
+  }
 
   let city = '';
-  if(response.data.address.city) city = response.data.address.city;
-  if(response.data.address.town) city = response.data.address.town;
-  if(response.data.address.village) city = response.data.address.village;
+  if (response.data.address.city) city = response.data.address.city;
+  if (response.data.address.town) city = response.data.address.town;
+  if (response.data.address.village) city = response.data.address.village;
 
   let county = '';
-  if(response.data.address.county) county = response.data.address.county;
-  if(response.data.address.city_district) county = response.data.address.city_district;
-  else if(response.data.address.district) county = response.data.address.district;
+  if (response.data.address.county) county = response.data.address.county;
+  if (response.data.address.city_district)
+    county = response.data.address.city_district;
+  else if (response.data.address.district)
+    county = response.data.address.district;
+
+  //
 
   return {
     adresaLinie1,
@@ -44,6 +65,6 @@ export const osmReverseLookup = async (props: Props): Promise<reportedLocation> 
     localitate: city,
     judet: county,
     lat,
-    lng
-  }
-}
+    lng,
+  };
+};
